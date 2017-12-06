ba6f04a949f3d4859ffd84c1be340bf77f7bc00d
David Ostler <davidtostler@gmail.com>
2017-12-05 22:47:26 -0700

    Add Firebase Added Firebase to the package and the api stuff to the environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA7zMMsNrZKGCJguBYIKWRPs1FEQzTJBQ8',
+    authDomain: 'workout-app-3fafb.firebaseapp.com',
+    databaseURL: 'https://workout-app-3fafb.firebaseio.com/',
+    projectId: 'workout-app-3fafb',
+    storageBucket: 'workout-app-3fafb.appspot.com',
+    messagingSenderId: '717857879930'
+  }
 };
